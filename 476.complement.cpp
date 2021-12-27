153971e55d6cb208a4327502c804cafb47295317
Naoyuki <zhaonzh@gmail.com>
2021-12-27 15:50:31 +0800

    bitwise xor

---
+++ 476.complement.cpp
@@ -0,0 +1,24 @@
+#include "header.hpp"
+class Solution
+{
+public:
+    int findComplement(int num)
+    {
+        int mask = 1;
+        while (mask < num)
+        {
+            // right shift once and plus one
+            // 1 -> 11 -> 111 -> 1111 -> 11111 -> 111111
+            mask = (mask << 1) + 1;
+        }
+        // doing a bitwise xor
+        // 111 xor 101 = 010
+        return mask ^ num;
+    }
+};
+int main()
+{
+    Solution s;
+    cout << s.findComplement(5) << endl;
+    return 0;
+}
