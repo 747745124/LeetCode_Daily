4d263695338131b4a18d431903ae500184de1368
Naoyuki <zhaonzh@gmail.com>
2022-12-18 21:13:16 -0500

    12.18 daily bfs

---
+++ 1971.isPath.cpp
@@ -0,0 +1,38 @@
+#include "header.hpp"
+class Solution
+{
+public:
+    bool validPath(int n, vector<vector<int>> &edges, int source, int destination)
+    {
+        vector<vector<int>> graph(n);
+        for (const auto &edge : edges)
+        {
+            graph[edge[0]].push_back(edge[1]);
+            graph[edge[1]].push_back(edge[0]);
+        }
+
+        bitset<20000> isVisited;
+        queue<int> q;
+
+        q.push(source);
+        while (!q.empty())
+        {
+
+            auto node = q.front();
+            q.pop();
+            isVisited.set(node);
+            if (node == destination)
+                return true;
+
+            for (const auto &dest : graph[node])
+            {
+                if (!isVisited.test(dest))
+                {
+                    q.push(dest);
+                }
+            }
+        }
+
+        return false;
+    }
+};
