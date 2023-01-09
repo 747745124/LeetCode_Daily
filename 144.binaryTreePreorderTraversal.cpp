2d4d645805df9db33a245b5e9c0a281ba6322724
Naoyuki <zhaonzh@gmail.com>
2023-01-08 20:49:43 -0800

    preorder

--- 144.binaryTreePreorderTraversal.cpp
+++ 144.binaryTreePreorderTraversal.cpp
@@ -15,5 +15,22 @@ class Solution
 public:
     vector<int> preorderTraversal(TreeNode *root)
     {
+
+        vector<int> result;
+        if (root == nullptr)
+            return result;
+        stack<TreeNode *> s;
+        s.push(root);
+        while (!s.empty())
+        {
+            TreeNode *node = s.top();
+            s.pop();
+            result.push_back(node->val);
+            if (node->right != nullptr)
+                s.push(node->right);
+            if (node->left != nullptr)
+                s.push(node->left);
+        }
+        return result;
     }
 };
